5b1c1dbeffcb4dfcb2c52dff89878d8b0be7edfa
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2025-03-21 16:18:40 -0500

    created table book_stock

--- create-tables.sql
+++ create-tables.sql
@@ -1,5 +1,10 @@
+DROP DATABASE IF EXISTS book_boutique;
 CREATE DATABASE book_boutique;
 USE book_boutique;
+CREATE TABLE book_series (
+	series_id INT PRIMARY KEY AUTO_INCREMENT,
+    name NVARCHAR(500) NOT NULL
+);
 CREATE TABLE books (
 	book_id INT PRIMARY KEY auto_increment,
     title NVARCHAR(1000) NOT NULL,
@@ -7,10 +12,12 @@ CREATE TABLE books (
     isbn DECIMAL(13, 0) NOT NULL,
 	release_date DATE NOT NULL,
     price DECIMAL(7,2) NOT NULL,
-    series_id INT, -- foreign key
+    series_id INT REFERENCES book_series(series_id),
     language ENUM("English", "Spanish", "Arabic", "French", "Japanese", "Chinese")
 );
-CREATE TABLE book_series (
-	series_id INT PRIMARY KEY AUTO_INCREMENT,
-    name NVARCHAR(500) NOT NULL
+CREATE TABLE book_stock (
+	stock_id INT PRIMARY KEY,
+    book_id INT NOT NULL UNIQUE REFERENCES books(book_id),
+    location_id INT NOT NULL UNIQUE, -- foreign key
+    quantity INT NOT NULL DEFAULT 0
 );
