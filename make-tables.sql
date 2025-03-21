9fe286fece96fa1c32ff8cda749eb132f934bf65
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2025-03-21 15:57:54 -0500

    Finished book table

--- make-tables.sql
+++ make-tables.sql
@@ -2,5 +2,11 @@ CREATE DATABASE book_boutique;
 USE book_boutique;
 CREATE TABLE books (
 	book_id INT PRIMARY KEY auto_increment,
-    title NVARCHAR(1000) NOT NULL
+    title NVARCHAR(1000) NOT NULL,
+    author_name NVARCHAR(200) NOT NULL,
+    isbn DECIMAL(13, 0) NOT NULL,
+	release_date DATE NOT NULL,
+    price DECIMAL(7,2) NOT NULL,
+    series_id INT, -- foreign key
+    language ENUM("English", "Spanish", "Arabic", "French", "Japanese", "Chinese")
 )
