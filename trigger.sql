7d8ec0ee6fba2087a3186a8c900030a286ef774b
JoyJoita <115607448+JoyJoita@users.noreply.github.com>
2025-04-11 16:36:42 -0500

    Update trigger.sql
    Complete trigger function.

--- trigger.sql
+++ trigger.sql
@@ -1,10 +1,15 @@
+-- Enforces the business rule that whenever a book is sold (i.e., added to an order), 
+-- its quantity is automatically reduced to reflect the updated stock.
+
+USE book_boutique;
+
 DELIMITER //
 CREATE TRIGGER tr_update_book_stock
 AFTER INSERT ON order_contents
 FOR EACH ROW
 BEGIN 
   UPDATE book_stock
-  SET book_stock.quantity = book_stock.quantity - 1
-  WHERE order_contents.book_id = book_stock.book_id;
+  SET quantity = quantity - NEW.quantity
+  WHERE book_id = NEW.book_id;
 END //
 DELIMITER ;
