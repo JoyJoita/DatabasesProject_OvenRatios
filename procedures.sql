307335f0e5757db801341a26796a1770738fbd7f
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2025-04-11 16:17:26 -0500

    Wrote a procedure to remove orphan records from order_contents

--- procedures.sql
+++ procedures.sql
@@ -18,4 +18,16 @@ Delimiter ;
 
 CALL p_checkForOrderOrphans;
 
-
+-- removes order contents entries for orders that no longer exist
+DROP PROCEDURE IF EXISTS p_remove_order_contents_orphans;
+DELIMITER //
+CREATE PROCEDURE p_remove_order_contents_orphans()
+BEGIN
+	DELETE FROM order_contents
+    WHERE order_id NOT IN (
+		SELECT order_id
+        FROM orders
+	);
+END //
+DELIMITER ;
+CALL p_remove_order_contents_orphans()
