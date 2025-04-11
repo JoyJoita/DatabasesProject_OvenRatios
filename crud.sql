8ab787334c898a27233af41990ecef4ef27d8d30
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2025-04-11 15:55:31 -0500

    Update crud.sql

--- crud.sql
+++ crud.sql
@@ -1,2 +1,19 @@
+USE book_boutique;
+
+-- A customer deletes their account
 DELETE FROM customers
 WHERE customer_id = 1;
+
+-- A price changes
+UPDATE books
+SET price = 13
+WHERE book_id = 2;
+
+-- A new shipment comes in
+UPDATE book_stock
+SET quantity = quantity + 200
+WHERE stock_id = 3;
+
+-- Our store closes because nobody shops there because we only stock 3 books
+DELETE FROM locations
+WHERE location_id = 1;
