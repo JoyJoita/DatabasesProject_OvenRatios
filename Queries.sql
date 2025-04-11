0b882b3a72deb27b7dd30dac331972dd0bf60ea3
Angela Oommen <angela.oommen21@gmail.com>
2025-04-11 16:23:57 -0500

    added queries

--- Queries.sql
+++ Queries.sql
@@ -17,3 +17,36 @@ ON bg.genre_id = g.genre_id
 LEFT JOIN book_series ser
 ON b.series_id = ser.series_id
 GROUP BY b.book_id;
+
+
+-- Total Revenue by Genre
+SELECT g.genre_name, SUM(oc.quantity * b.price) AS genre_revenue
+FROM order_contents oc
+JOIN books b ON oc.book_id = b.book_id
+JOIN book_genres bg ON b.book_id = bg.book_id
+JOIN genres g ON bg.genre_id = g.genre_id
+GROUP BY g.genre_name
+ORDER BY genre_revenue DESC;
+
+
+-- Top Customers by Purchase Value
+SELECT c.name,
+COUNT(o.order_id) AS orders_placed,
+SUM(o.total_cost) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id
+ORDER BY total_spent DESC;
+
+
+-- Books that were not sold
+SELECT b.title, b.author_name, b.price, b.release_date
+FROM books b
+LEFT JOIN order_contents oc ON b.book_id = oc.book_id
+WHERE oc.book_id IS NULL
+ORDER BY b.release_date DESC;
+
+
+
+
+
