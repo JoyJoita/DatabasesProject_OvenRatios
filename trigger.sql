615072963005c8c6d40d0cc8aa8d775059e9c1e4
JoyJoita <115607448+JoyJoita@users.noreply.github.com>
2025-04-21 14:56:42 -0500

    Update trigger.sql
    Create order

--- trigger.sql
+++ trigger.sql
@@ -17,8 +17,11 @@ DELIMITER ;
 -- BEFORE A ORDER IS PLACED
 SELECT * FROM book_stock;
 
+INSERT INTO orders (customer_id, order_date, total_cost) VALUES
+(2, '2025-04-21', 65.94);
+  
 INSERT INTO order_contents (order_id, book_id, quantity) VALUES
-(1, 1, 3);
+(33, 1, 3);
 
 -- AFTER A ORDER IS PLACED
 SELECT * FROM book_stock;
